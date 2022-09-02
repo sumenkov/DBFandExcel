cc37d591a36e8484842c1678e468cd9f9be5646c
sumenkovdv <sumenkov.dv@yandex.ru>
2022-09-02 09:38:08 +0700

    first

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
+    /*
+    1. директории для DBF и xls
+    2. сравнение списков в директориях + дата создания/изменения
+    3. графичиский интерфейс с гридом и термометром
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
